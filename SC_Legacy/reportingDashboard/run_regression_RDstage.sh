be79c3388f014df3818bccbf4b7b1ef45d68e295
Jasmine Ma <jasmine.ma@bydeluxe.com>
2021-06-03 12:58:26 -0700

    notes

--- SC_Legacy/reportingDashboard/run_regression_RDstage.sh
+++ SC_Legacy/reportingDashboard/run_regression_RDstage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/workspace
+#cd ~/workspace
 source ./virtualenv_SC/Script/activate
 cd ./Supplychain_Legacy/reportingDashboard
 pytest -v -s --junit-xml=pytest_report.xml -m "regression" --env=stage --browser=chrome --headless=true ./testsuites/
