cef957a8ce4396c3657c671b3f8e920c1a6a7427
Jasmine Ma <50595520+Jasmine-Ma@users.noreply.github.com>
2021-06-08 15:27:42 -0700

    Update connect_dbb.py

--- SC_Legacy/venue/utils/connect_dbb.py
+++ SC_Legacy/venue/utils/connect_dbb.py
@@ -4,10 +4,10 @@ import psycopg2
 
 def connect_stage_dbb():
     conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=10.6.43.12;'
+                      'Server=No;'
                       'Database=SonyDBB;'
                       'UID=dbb;'
-                      'PWD=ws5#VNd%jxz9;'
+                      'PWD=No'
                       'Trusted connection=NO')
     cursor = conn.cursor()
     cursor.execute('SELECT top 10 * FROM SonyDBB.dbo.title')
@@ -21,9 +21,9 @@ def connect_stage_dbb():
 
 
 def connect_stage_postgres():
-    conn = psycopg2.connect(user="supplychainstgdbing",
-                            password="caTRIDEmiA",
-                            host="10.6.40.101",
+    conn = psycopg2.connect(user="",
+                            password="",
+                            host="ips",
                             database="ingestor")
     cursor = conn.cursor()
     postgres_Select = "SELECT * From public.watchfolder"
